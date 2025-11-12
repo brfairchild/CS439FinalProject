9e09abfcfdeb1eacd1aba6ff767e4125babe4521
Brayden Fairchild <153120388+brfairchild@users.noreply.github.com>
2025-11-12 00:48:00 -0500

    Init Commit

--- obj/Debug/net9.0/CS439FinalProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/CS439FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS439FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09cfd327d0d89a047fbd9fefc57f70ff30512f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS439FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS439FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
